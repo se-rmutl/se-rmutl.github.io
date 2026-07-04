127f3ce786c5d11c7f35ee3dcb106aef9f519760
tkeatkaew <tkeatkaew@gmail.com>
2026-07-04 18:23:59 +0700

    lab02 added

--- engse203/labs/engse203-lab02-solution/vite.config.js
+++ engse203/labs/engse203-lab02-solution/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 
 // For deployment, change this to the exact repository name on GitHub.
+
+const repositoryPrefix = "engse203/labs";
 const repositoryName = "engse203-lab02-solution";
 
+
 export default defineConfig({
-  base: `/${repositoryName}/`,
+  base: `/${repositoryPrefix}/`+`${repositoryName}/`,
   build: {
     outDir: "docs",
     emptyOutDir: true,
